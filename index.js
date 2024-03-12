298e0909d76d59b0eb8b06d4c6c839646101130c
Narendar2024 <narendarkorem471@gmail.com>
2024-03-12 11:31:09 +0530

    user authentication completed

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const User = require('./models/User')
+const User = require('./models/User');
+const bccrypt = require('bcryptjs');
+
 
 const app = express();
 
@@ -40,6 +42,14 @@ app.use(session({
     store: store
 }));
 
+const checkAuth = (req, res, next) => {
+    if (req.session.isAuthenticated) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
 
 app.get('/signup', (req, res) => {
     res.render('register');
@@ -50,28 +60,67 @@ app.get('/login', (req, res) => {
 });
 
 
-app.get('/dashboard', (req, res) => {
+app.get('/dashboard', checkAuth, (req, res) => {
     res.render('welcome');
 });
 
-app.post('/register', async (req, res) => {
-    const { username, email, password } = req.body;
-    try {
-        const newUser = new User({
-            username, email, password
-        })
-        await newUser.save();
-        res.redirect('/login');
-    } catch (err) {
-        console.error(err);
-        res.redirect('/signup');
-    }
-})
+// app.post('/register', async (req, res) => {
+//     const { username, email, password } = req.body;
+//     try {
+//         const newUser = new User({
+//             username, email, password
+//         })
+//         await newUser.save();
+//         req.session.person = username
+//         res.redirect('/login');
+//     } catch (err) {
+//         console.error(err);
+//         res.redirect('/signup');
+//     }
+// })
 
 // app.get('/', (req, res) => {
 //     res.send(`Server is running at ${PORT}`);
 // });
 
+app.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.redirect('/signup');
+        }
+        const hashPassword = await bccrypt.hash(password, 12);
+        user = new User({
+            username,
+            email,
+            password: hashPassword
+        });
+        req.session.person = user.username
+        await user.save();
+        res.redirect('/login');
+    } catch (error) {
+        console.error(`${error}`);
+    }
+})
+
+app.post('/user-login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.redirect('/signup')
+        }
+        const checkPassword = await bccrypt.compare(password, user.password);
+        if (!checkPassword) {
+            return res.redirect('/signup');
+        }
+        req.session.isAuthenticated = true
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.error(`${error}`);
+    }
+})
 app.listen(PORT, () => {
     console.log(`Server running successfully @ ${PORT}`);
 });
